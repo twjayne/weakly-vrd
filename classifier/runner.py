fbefa013f600ad186e5fe4473363c66a203be294
Markham <quinesquines@gmail.com>
2018-07-12 11:16:04 -0700

    Concatenate features in new variables to feed to classifier

--- classifier/runner.py
+++ classifier/runner.py
@@ -1,7 +1,11 @@
 import torch
 import os, datetime
-
+import classifier.classifier as classifier
+import classifier.solver as solver
 
 LEARNING_RATE = os.environ.get('lr', None)
+EXTRA_DIMENSIONS = os.environ.get('extra', 500)
+N_PREDCIATE_CLASSES = 70
 
+model = classifier.Classifier(1000, 2000, EXTRA_DIMENSIONS, 4096, N_PREDCIATE_CLASSES)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
--- classifier/solver.py
+++ classifier/solver.py
@@ -4,18 +4,28 @@ class Solver(generic_solver.GenericSolver):
 	
 	def _train_step(self, iteration, batch):
 		self.model.train()
-		# Split up batch
-		batch_X1 = 
-		batch_X2 = 
-		batch_Y  = 
+		# Allocate variables to hold concatenated inputs (and then feed into the model layers)
+		N = len(batch[LBL_PREDICATE])
+		if 'stage2_X' not in dir(self):
+			F = model.stage2[0].in_features
+			self.stage2_X = torch.autograd.Variable(torch.Tensor( N, F ))
+		if 'stage1_X' not in dir(self):
+			F = model.stage1[0].in_features
+			self.stage1_X = torch.autograd.Variable(torch.Tensor( N, F ))
 		# Forward pass
-		h1 = self.model.block1.forward(batch_X1)
-		batch_X2[:len(h1)] = h1
-		prediction = self.model.block2.forward(batch_X2)
-		loss = self.loss_fn(prediction, batch_Y)
+		self.stage1_X.data = torch.cat((batch[LBL_APPEARANCE], batch[LBL_SPATIAL]), dim=1)
+		h1 = self.model.block1.forward(self.stage1_X)
+		self.stage2_X.data = torch.cat((h1, batch[LBL_STAGE2]), dim=1)
+		prediction = self.model.block2.forward(self.stage2_X)
+		loss = self.loss_fn(prediction, batch[LBL_PREDICATE])
 		# Take step
 		self.loss_history[iteration] = loss
 		self.optimizer.zero_grad()
 		self.loss_history[iteration].backward()
 		optimizer.step()
 		return self.loss_history[iteration]
+
+LBL_PREDICATE = 'predicate'
+LBL_APPEARANCE = 'spatial'
+LBL_SPATIAL = 'spatial'
+LBL_STAGE2 = 'extra'
