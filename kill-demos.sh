4e2a781f0c21eca93e422891b06c48a5d2ac1c2c
Markham <quinesquines@gmail.com>
2018-08-16 15:57:44 -0700

    use args in kill-demos.sh

--- kill-demos.sh
+++ kill-demos.sh
@@ -3,11 +3,17 @@
 function search_and_kill ()
 {
 	echo grepping $1
-	ps ux | grep "$1" | grep -v grep | tr -s ' ' | cut -d' ' -f2 | while read pid; do
+	ps ux | grep "$1" | grep -v grep | grep -v kill-demos | tr -s ' ' | cut -d' ' -f2 | while read pid; do
 		ps -p $pid | tail -n1
 		[[ -n $pid ]] && kill $pid
 	done
 }
 
-search_and_kill 'run.sh'
-search_and_kill 'python demo'
+if (($#)); then
+	for arg in "${@:1}"; do
+		search_and_kill "$arg"
+	done
+else
+	search_and_kill 'run.sh'
+	search_and_kill 'python demo'
+fi
