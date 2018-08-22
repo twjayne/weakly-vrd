434ef8e71c9a36f94abab3694cafc0139658009f
Markham <quinesquines@gmail.com>
2018-08-22 14:44:06 -0700

    working faster r-cnn

--- unrel-pretrained/test_model.py
+++ unrel-pretrained/test_model.py
@@ -16,53 +16,9 @@ import torchvision.models as models
 import skimage.io
 import numpy as np
 
-######
-# FC layers
-######
+from unrel_model import Model, TRANSFORM
 
-fc_state_dict = torch.load('linear.prototxt.statedict.pth')
-classifier_keys = ['fc6.1', 'fc7.1', 'cls_score.1']
-map_keys = ['0', '3', '6']
-
-def classifier_layers():
-	for i in range(3):
-		geom = fc_state_dict['%s.weight' % classifier_keys[i]].shape[::-1]
-		yield nn.Linear(*geom)
-		if i < 3-1:
-			yield nn.ReLU()
-			yield nn.Dropout()
-
-classifier = nn.Sequential(*[x for x in classifier_layers()])
-modified_dict = {}
-for i,pfx in enumerate(classifier_keys):
-	for kind in ('.weight', '.bias'):
-		modified_dict[map_keys[i]+kind] = fc_state_dict[pfx+kind]
-classifier.load_state_dict(modified_dict)
-print(classifier)
-
-######
-# Conv layers
-######
-
-vgg = models.vgg16()
-features = vgg.features
-conv_state_dict = torch.load('conv.prototxt.statedict.pth')
-keys = zip(features.state_dict().keys(), conv_state_dict.keys())
-modified_dict = {vk: conv_state_dict[ck] for vk, ck in keys}
-features.load_state_dict(modified_dict)
-print(features)
-
-
-######
-# Test
-######
-
-features = features.cuda()
-classifer = classifier.cuda()
-
-features.eval()
-classifier.eval()
-
+import pdb
 
 xform = torchvision.transforms.Compose([
 	torchvision.transforms.Resize((224,224)),
@@ -77,31 +33,55 @@ fetcher = unrel.Builder().split('train', 'annotated')
 obj_names = ['<background>'] + fetcher.obj_names
 
 
-def run_bbs(im_id=890, show=False, verbosity=0):
-	global cts
-	if im_id % 20 == 0: print('running img', im_id)
-	entities = fetcher.entities(im_id)
-	im_data = fetcher.image(im_id)
-	for ent in entities:
-		t = xform(ent.crop(im_data)).cuda()
-		t *= 255 # Accomodate for the fact that the model comes from caffe
-		t = t.index_select(0, BGR) # Accomodate for the fact that the model comes from caffe
-		t -= MEAN # Subtract mean
-		t.unsqueeze_(0)
-		x = features(t)
-		y = classifier(x.view(-1))
-		cat_id = torch.argmax(y)
-		_,ranks = y.sort(0, True)
-		rank = (ranks == int(ent.obj_cat)).nonzero()[0,0]
-		cts[rank:] += 1 # Increment all counts >= rank, indicating that the corret result is in the top k predictions
-		if verbosity >= 1:
-			print('TOP5: %2d\tPREDICTION: (#%d) %12s\t GT: (#%d) %12s' % (in_topk, cat_id, obj_names[cat_id], ent.obj_cat, ent.name()))
-		if show:
-			t.squeeze_(0)
-			toimg(t.cpu()).show()
+modelpath = 'netmodel.pth'
+if os.path.exists(modelpath):
+	model = torch.load(modelpath)
+else:
+	model = Model()
+	torch.save(model, modelpath)
+model = model.cuda()
 
 cts = torch.zeros(101).int()
-for i in range(1,4000): run_bbs(i)
+for i in range(1,100):
+	imdata = TRANSFORM(fetcher.image(i)).cuda()
+	bbs = [list(ent.bb()) for ent in fetcher.entities(i)]
+	print('IMID %4d' % i)
+	if not fetcher.entities(i): continue
+	scoresets = model(imdata, torch.Tensor(bbs).cuda())
+	for j, ent in enumerate(fetcher.entities(i)):
+		scores = scoresets[j,:]
+		cat_id = torch.argmax(scores)
+		_,ranks = scores.sort(0, True)
+		rank = (ranks == int(ent.obj_cat)).nonzero()[0,0]
+		cts[rank:] += 1
+		print('RANK: %3d\tPREDICTION: (#%3d) %12s\t GT: (#%3d) %12s' % (rank, cat_id, obj_names[cat_id], ent.obj_cat, ent.name()))
+
+
+
 print('cts', cts)
 plt.bar( torch.arange(len(cts)), cts.float() / cts[-1].float(), width=1.0 )
 plt.show()
+
+
+# def run_bbs(im_id=890, show=False, verbosity=0):
+# 	global cts
+# 	if im_id % 20 == 0: print('running img', im_id)
+# 	entities = fetcher.entities(im_id)
+# 	im_data = fetcher.image(im_id)
+# 	for ent in entities:
+# 		t = xform(ent.crop(im_data)).cuda()
+# 		t *= 255 # Accomodate for the fact that the model comes from caffe
+# 		t = t.index_select(0, BGR) # Accomodate for the fact that the model comes from caffe
+# 		t -= MEAN # Subtract mean
+# 		t.unsqueeze_(0)
+# 		x = features(t)
+# 		y = classifier(x.view(-1))
+# 		cat_id = torch.argmax(y)
+# 		_,ranks = y.sort(0, True)
+# 		rank = (ranks == int(ent.obj_cat)).nonzero()[0,0]
+# 		cts[rank:] += 1 # Increment all counts >= rank, indicating that the corret result is in the top k predictions
+# 		if verbosity >= 1:
+# 			print('TOP5: %2d\tPREDICTION: (#%d) %12s\t GT: (#%d) %12s' % (in_topk, cat_id, obj_names[cat_id], ent.obj_cat, ent.name()))
+# 		if show:
+# 			t.squeeze_(0)
+# 			toimg(t.cpu()).show()
---
+++ unrel-pretrained/unrel_model.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# This vgg16 model comes from the caffe files provided by the writers of
+# "Weakly-supervised learning of visual relations." See
+# https://www.di.ens.fr/willow/research/unrel/
+
+# You must add faster-rcnn.pytorch-master/lib to the PYTHONPATH so that this module
+# can make use of faster-rcnn.
+
+# If you get error
+# RuntimeError: cuda runtime error (77) : an illegal memory access was encountered at /opt/conda/conda-bld/pytorch_1524584710464/work/aten/src/THC/generic/THCTensorCopy.c:70
+# then run with CUDA_LAUNCH_BLOCKING=1
+
+import torch
+import torch.nn as nn
+import torchvision.models
+import torchvision.transforms
+
+# Import local modules
+import sys, os
+sys.path.append(os.path.realpath(os.path.join(__file__, '../..')))
+import util.unrel_data as unrel
+
+# Import faster-rcnn modules
+from model.roi_pooling.modules.roi_pool import _RoIPooling
+from model.utils.config import cfg
+
+
+class Model(nn.Module):
+	def __init__(self, features=None, classifier=None, **kwargs):
+		super(Model, self).__init__()
+		self.verbose = kwargs.get('verbose', False)
+		self._init_features(features or './conv.prototxt.statedict.pth')
+		self._init_classifier(classifier or './linear.prototxt.statedict.pth')
+		self.RoIPooling = _RoIPooling(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/32.0) # This needs to be the ratio of imdata.shape to the shape of the feature map at the end of the convolutional layers. This is architecture-dependent, not image-dependent (though a pixel here or there can cause some small shift in the true ratio).
+		self._batch_index = 0.
+
+	def forward(self, imdata, bbs):
+		# Extract features
+		x = self.features(imdata)
+		# Get ROIs
+		rois = self._rois(x, bbs)
+		# Classify ROIs
+		scoresets = self.classifier(rois.view(bbs.shape[0],-1))
+		return scoresets
+
+	def _rois(self, features, bbs):
+		batch_index = self._batch_index * torch.ones(bbs.shape[0],1)
+		if features.is_cuda: batch_index = batch_index.cuda()
+		boxes_plus = torch.cat((batch_index, bbs), 1)
+		if features.is_cuda: boxes_plus = boxes_plus.cuda()
+		return self.RoIPooling(features, boxes_plus)
+
+	def _load_state_dict(self, path_or_dict):
+		return path_or_dict if isinstance(path_or_dict, dict) else torch.load(path_or_dict)
+
+	######
+	# FC layers
+	######
+	def _init_classifier(self, path_or_dict=None):
+		print('Loading FC layers...')
+		state_dict = self._load_state_dict(path_or_dict)
+		src_keys = ['fc6.1', 'fc7.1', 'cls_score.1']
+		map_keys = ['0', '3', '6']
+		def classifier_layers():
+			for i in range(3):
+				geom = state_dict['%s.weight' % src_keys[i]].shape[::-1]
+				yield nn.Linear(*geom)
+				if i < 3-1:
+					yield nn.ReLU()
+					yield nn.Dropout()
+		classifier = nn.Sequential(*[x for x in classifier_layers()])
+		mapped_dict = {}
+		for i,pfx in enumerate(src_keys):
+			for kind in ('.weight', '.bias'):
+				mapped_dict[map_keys[i]+kind] = state_dict[pfx+kind]
+		classifier.load_state_dict(mapped_dict)
+		if self.verbose: print(classifier)
+		self.classifier = classifier
+
+	######
+	# Conv layers
+	######
+	def _init_features(self, path_or_dict=None):
+		print('Loading Conv layers...')
+		vgg = torchvision.models.vgg16()
+		features = vgg.features
+		state_dict = self._load_state_dict(path_or_dict)
+		keys = zip(features.state_dict().keys(), state_dict.keys())
+		mapped_dict = {vk: state_dict[ck] for vk, ck in keys}
+		features.load_state_dict(mapped_dict)
+		if self.verbose: print(features)
+		self.features = features
+
+
+MEAN = torch.FloatTensor(list(reversed([123.68, 116.779, 103.939]))).view(1,3,1,1) # Reversed b/c caffe does BGR
+BGR_indices = torch.LongTensor((2,1,0))
+_transform_caffe_scale = torchvision.transforms.Lambda(lambda ten: ten * 255)
+_transform_caffe_BGR = torchvision.transforms.Lambda(lambda ten: ten.index_select(0, BGR_indices))
+_transform_subtract_mean = torchvision.transforms.Lambda(lambda ten: ten - MEAN)
+
+TRANSFORM = torchvision.transforms.Compose([
+	torchvision.transforms.ToTensor(),
+	_transform_caffe_scale,
+	_transform_caffe_BGR,
+	_transform_subtract_mean,
+])
+
+if __name__ == '__main__':
+	modelpath = 'netmodel.pth'
+	if os.path.exists(modelpath):
+		model = torch.load(modelpath)
+	else:
+		model = Model()
+		torch.save(model, modelpath)
+	model = model.cuda()
+
+	fetcher = unrel.Builder().split('train', 'annotated')
+	obj_names = ['<background>'] + fetcher.obj_names
+
+	imid = 3
+	imdata = TRANSFORM(fetcher.image(imid)).cuda()
+	bbs = [list(ent.bb()) for ent in fetcher.entities(imid)]
+	ss = model(imdata, torch.Tensor(bbs).cuda())
+
+	print(ss.argmax(1).cpu().numpy().tolist())
+	print([ent.obj_cat for ent in fetcher.entities(imid)])
+	print([ent.name() for ent in fetcher.entities(imid)])
