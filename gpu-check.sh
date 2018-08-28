33c42a3b48cdd5f1c9607b1da6a1e7cf236c9143
Markham <quinesquines@gmail.com>
2018-08-28 10:57:22 -0700

    add output fields GPU and GMEM to gpu-check script

--- gpu-check.sh
+++ gpu-check.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Get width of terminal
+COLS=$(( $(tput cols) - 2 ))
+# Get nvidia-smi data
+SMI=$(nvidia-smi | awk 'x == 1 {print} /PID/ {x = 1}' | tr ' ' $'\t' | tr -s $'\t')
+# Get ps data
+PS=$(cut -f3 <<< "$SMI" | grep -P '[0-9]+' | xargs ps -o 'pid,user,pcpu,pmem,bsdtime,args' -p)
 
-nvidia-smi | awk 'x == 1 {print $0}; /PID/ {x = 1}' \
-| grep -P '\d' \
-| tr -s ' ' \
-| cut -d' ' -f3 \
-| xargs ps -l -p
+# Print HEADER
+printf '%3s %7s ' GPU GMEM
+head -n1 <<< "$PS"
+
+# Print BODY ROWS
+while read -r row; do
+	pid=$(tr -s ' ' <<< "$row" | cut -d' ' -f1)
+	while read -a nvidia; do
+		printf '%3s %7s %s\n' "${nvidia[0]}" "${nvidia[1]}" "$row" | cut -c1-$COLS
+	done < <(grep $pid <<< "$SMI" | cut -f2,6)
+done < <(tail -n+2 <<< "$PS")
