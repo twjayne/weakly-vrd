Use sampler even with FauxDataLoader. Fix backprop (to accumulate loss across images in batch) for faster r-cnn

classifier/generic_solver.py
@@ -47,8 +47,12 @@ class GenericSolver:
 	
 	# @arg trainloader should be a Dataloader
 	# @arg testloaders should be a Dataloaders
-	def train(self, trainloader, testloader, *additional_testloaders):
+	def train(self, trainloader, *testloaders):
+		# Init dataloaders
 		self.init_train(trainloader)
+		testloader = testloaders[0] if len(testloaders) else None
+		additional_testloaders = testloaders[1:]
+		# Iterate
 		for self.epoch in range(self.num_epochs):
 			for batch_i, batch in enumerate(trainloader):
 				# Train
@@ -58,7 +62,7 @@ class GenericSolver:
 				if self.verbose and batch_i % self.print_every == 0:
 					self._print(loss, 'TRAIN')
 				if self.scheduler and testloader is None:
-					self.scheduler.step(loss)
+					self.scheduler.step(loss.item())
 				# Test
 				if self.iteration % self.test_every == 0:
 					if testloader: self._test(testloader, True)
@@ -75,7 +79,7 @@ class GenericSolver:
 	def _train_step(self, batch):
 		self.model.train()
 		self.optimizer.zero_grad()
-		loss = self._calc_loss(batch)
+		loss = self.loss_calculator.calc(batch)
 		loss.backward()
 		self.loss_history[self.iteration] = float(loss.data)
 		self.optimizer.step()
@@ -84,9 +88,10 @@ class GenericSolver:
 	def _test(self, testloader, is_primary, do_recall=True):
 		self.model.eval()
 		loss = self.loss_calculator.calc(testloader, do_recall)
-		self._print(loss, testloader.dataset.name or 'TEST')
-		if do_recall: print('\tRECALL %f' % self.loss_calculator.recall)
-		if self.scheduler: self.scheduler.step(loss)
+		extra_strings = []
+		if do_recall: extra_strings.append('rec %.2f' % self.loss_calculator.recall)
+		self._print(loss, testloader.dataset.name or 'TEST', *extra_strings)
+		if self.scheduler: self.scheduler.step(loss.item())
 		if self.opts.get('save_best', False) and self.acc > self.best_acc:
 			self.save_checkpoint('best.pth')
 			self.best_acc = self.acc
@@ -104,15 +109,20 @@ class GenericSolver:
 		torch.save({
 			'epoch': self.epoch,
 			'iteration': self.iteration,
+			'model_type': str(type(self.model)),
 			'state_dict': self.model.state_dict(),
 			'optimizer': self.optimizer.state_dict(),
+			'optimizer_type': str(type(self.optimizer)),
 			}, os.path.join(self.outdir or '', filename))
 
-	def _print(self, loss, dataname='TRAIN'):
-		print('%12s (ep %3d: %5d/%d) loss %e\tacc %.3f' % (dataname, self.epoch, self.iteration, self.num_iterations, loss, self.loss_calculator.acc))
+	def _print(self, loss, dataname='TRAIN', *extra_strings):
+		text = '%12s (ep %3d: %5d/%d) loss %e\tacc %.3f' % (dataname, self.epoch, self.iteration, self.num_iterations, loss, self.loss_calculator.acc)
+		for string in extra_strings: text += '\t%s' % string
+		print(text)
 
 	def debug(self):
 		print('%20s %s' % ('optimizer', str(self.optimizer),))
+		print('%20s %s' % ('scheduler', str(self.scheduler.state_dict() if self.scheduler else None),))
 		print('%20s %s' % ('model', str(self.model),))
 		print('%20s %s' % ('cuda', self.cuda,))
 		print('%20s %s' % ('supervision', self.supervision,))

classifier/loss_calculator.py
@@ -6,6 +6,9 @@ import util.gpu
 
 import pdb
 
+DTYPE = torch.float32
+DO_CUDA = True
+
 class LossCalculator(object):
 	# @arg input_key can be a lambda or a dictionary key
 	def __init__(self, model, **opts):
@@ -14,7 +17,7 @@ class LossCalculator(object):
 		self.target_key = opts.get('target_key', 'y')
 		self.loss_fn    = opts.get('loss', nn.CrossEntropyLoss())
 		self.recall_x   = opts.get('recall', 50)
-		self.loss_fn.cuda()
+		if DO_CUDA: self.loss_fn.cuda()
 
 	def calc(self, data, do_recall=False):
 		if isinstance(data, dict):

dataset/faster_rcnn.py
@@ -1,6 +1,7 @@
 import torch
 assert torch.__version__.startswith('0.4'), 'wanted version 0.4, got %s' % torch.__version__
 import torch.utils.data
+import torch.utils.data.sampler as torchsampler
 import numpy as np
 import scipy.io
 import os
@@ -60,41 +61,83 @@ class Dataset(torch.utils.data.Dataset):
 	def __iter__(self):
 		return _DataLoaderIter(self)
 
+# Behaves like a dataloader but doesn't stack all fields (because some of them, such as images, may be of differing dimensions)
 class FauxDataLoader(torch.utils.data.DataLoader):
-	def __init__(self, dataset, batch_size=1):
-		self.dataset = dataset
-		self.batch_size = batch_size
-		self.cur = 0
+	def __init__(self, dataset, **kwargs):
+		self.dataset    = dataset
+		self.sampler    = self._init_sampler(**kwargs)
+		self.batch_size = self.sampler.batch_size
+		self.iterator   = iter(self.sampler)
 	def __iter__(self):
 		return self
 	def __len__(self):
-		return int(np.ceil(len(self.dataset) / float(self.batch_size)))
+		return len([item for batch in self.sampler for item in batch])
 	def __next__(self):
-		remaining = len(self.dataset) - self.cur - 1
-		interval  = min(remaining, self.batch_size)
-		if interval == 0:
-			self.cur = 0
+		# Get batch
+		try:
+			batch = next(self.iterator)
+		except StopIteration:
+			self.iterator = iter(self.sampler)
 			raise StopIteration
-		else:
-			selection = [self.dataset[i] for i in range(self.cur, self.cur+interval)]
-			self.cur += interval
-			return {
-				'im_id':    [d['imid'] for d in selection],
-				'image':   [d['image'] for d in selection],
-				'bbs':     [d['bbs'] for d in selection],
-				'preds':   torch.cat([d['preds'] for d in selection]),
-				'spatial': torch.cat([d['spatial'] for d in selection]),
-				'N':       len(selection),
-			}
+		if isinstance(batch[0], int):
+			batch = [self.dataset[i] for i in batch]
+		# Return dict
+		return {
+			'im_id':    [d['imid'] for d in batch],
+			'image':   [d['image'] for d in batch],
+			'bbs':     [d['bbs'] for d in batch],
+			'preds':   torch.cat([d['preds'] for d in batch]),
+			'spatial': torch.cat([d['spatial'] for d in batch]),
+			'N':       len(batch),
+		}
+	def _init_sampler(self, **kwargs):
+		sampler = kwargs.get('sampler')
+		batch_size  = kwargs.get('batch_size', 1)
+		drop_last   = kwargs.get('drop_last', False)
+		if isinstance(sampler, torchsampler.BatchSampler):
+			return sampler
+		if sampler == None:
+			sampler = torchsampler.RandomSampler(self.dataset)
+		elif not isinstance(sampler, torchsampler.Sampler):
+			sampler = torchsampler.RandomSampler(sampler)
+		return torchsampler.BatchSampler(sampler, batch_size, drop_last)
 
+# Test this module
 if __name__ == '__main__':
+	N_TESTS = 4
+	passed = 0
 	dataset = Dataset(transform=unrel.TRANSFORM)
-	dataloader = FauxDataLoader(dataset, 2)
-	count = 0
-	for batch in dataloader:
+	# Test on a subset sampler
+	batch_sampler = torchsampler.SequentialSampler(range(14))
+	dataloader = FauxDataLoader(dataset, sampler=batch_sampler)
+	for batch_i, batch in enumerate(dataloader):
 		assert isinstance(batch['image'], list)
 		for image in batch['image']:
 			assert isinstance(image, torch.Tensor)
-			count += 1
-		print('dataset count %3d / %3d' % (dataloader.cur, len(dataset)))
-	print('OK')
+		print('dataset count %3d / %3d' % ((1+batch_i) * dataloader.sampler.batch_size, len(dataloader)))
+	passed += 1; print('OK %d/%d' % (passed, N_TESTS))
+	# Test on a batched subset sampler
+	batch_sampler = torchsampler.BatchSampler(torchsampler.SequentialSampler(range(14)), 3, False)
+	dataloader = FauxDataLoader(dataset, sampler=batch_sampler)
+	for batch_i, batch in enumerate(dataloader):
+		assert isinstance(batch['image'], list)
+		for image in batch['image']:
+			assert isinstance(image, torch.Tensor)
+		print('dataset count %3d / %3d' % ((1+batch_i) * dataloader.sampler.batch_size, len(dataloader)))
+	passed += 1; print('OK %d/%d' % (passed, N_TESTS))
+	# Test second time on same dataloader to ensure that reset works
+	for batch_i, batch in enumerate(dataloader):
+		assert isinstance(batch['image'], list)
+		for image in batch['image']:
+			assert isinstance(image, torch.Tensor)
+		print('dataset count %3d / %3d' % ((1+batch_i) * dataloader.sampler.batch_size, len(dataloader)))
+	passed += 1; print('OK %d/%d' % (passed, N_TESTS))
+	# Test without supplying sampler
+	dataloader = FauxDataLoader(dataset, batch_size=2)
+	for batch_i, batch in enumerate(dataloader):
+		assert isinstance(batch['image'], list)
+		for image in batch['image']:
+			assert isinstance(image, torch.Tensor)
+		if batch_i % 50 == 0:
+			print('dataset count %3d / %3d' % ((1+batch_i) * dataloader.sampler.batch_size, len(dataloader)))
+	passed += 1; print('OK %d/%d' % (passed, N_TESTS))

run/e2e_runner.py
@@ -8,8 +8,8 @@ import sys
 import os
 import torch
 import torch.nn as nn
-from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
+import torch.utils.data.sampler as sampler
 
 import dataset.faster_rcnn as data
 import unrel.unrel_model as unrel
@@ -20,9 +20,9 @@ from classifier.loss_calculator import LossCalculator
 import pdb
 
 parser = shared.parser
-parser.remove_option('-N')
+parser.add_option('--bp_every', dest='backprop_every', default=4, type='int') # Don't backprop on every 'batch'. Instead backprop after multiple batches.
 assert parser.has_option('--bs')
-parser.defaults['batch_size'] = 2
+parser.defaults['batch_size'] = 1
 assert parser.has_option('--tbs')
 parser.defaults['test_batch_size'] = 1
 assert parser.has_option('--outdir')
@@ -30,7 +30,7 @@ parser.defaults['outdir'] = 'log/e2e/vgg16'
 assert parser.has_option('--geom')
 parser.defaults['geom'] = '1400 1024 700 71'
 assert parser.has_option('--test_every')
-parser.defaults['test_every'] = 250
+parser.defaults['test_every'] = 2048
 
 class Model(unrel.Model):
 	def __init__(self, n_vis_features, *args, **kwargs):
@@ -51,13 +51,23 @@ class Model(unrel.Model):
 
 
 class Solver(generic_solver.GenericSolver):
+	def __init__(self, *args, **kwargs):
+		super(Solver, self).__init__(*args, **kwargs)
+		self.loss = 0
+		self.pairs_n = 0
+		self.verbose = False
+
 	def _train_step(self, batch):
 		self.model.train()
 		self.optimizer.zero_grad()
 		loss = self.loss_calculator.calc(batch)
-		self.loss_history[self.iteration] = float(loss.data)
-		if self.iteration and self.iteration % 50 == 0:
-			loss.backward()
+		self.pairs_n += batch['preds'].shape[0]
+		self.loss = self.loss + loss
+		if self.iteration and self.iteration % r.opts.backprop_every == 0: # If this is not the first iteration and we have enough iterations to merit a backprop
+			self.loss.backward()
+			self._print(self.loss / self.pairs_n, 'ACC_TRAIN')
+			self.pairs_n = 0
+			self.loss = 0
 			self.optimizer.step()
 		return loss
 
@@ -77,7 +87,7 @@ class Runner(shared.Runner):
 			self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.opts.lr)
 		if self.scheduler == None:
 			print('Building scheduler...')
-			self.scheduler = None if self.opts.no_scheduler else torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, verbose=True, patience=self.opts.patience)
+			self.scheduler = None if self.opts.no_scheduler else torch.optim.lr_scheduler.MultiStepLR(self.optimizer, [x * self.opts.backprop_every for x in [35, 75, 120, 200, 400, 600, 800]], 0.5)
 		if self.solver == None:
 			print('Building solver...')
 			loss_calculator = LossCalculator(self.model, input_key=lambda model, batch: model(batch), target_key='preds')
@@ -87,10 +97,19 @@ class Runner(shared.Runner):
 		transform = unrel.TRANSFORM
 		# Initialize trainset
 		self.trainset = data.Dataset(split='train', pairs='annotated', transform=transform)
-		self.trainloader = data.FauxDataLoader(self.trainset, self.opts.batch_size)
+		if self.opts.train_size:
+			print('Using subset of %d from train_set' % self.opts.train_size)
+			batch_sampler = sampler.SequentialSampler(range(self.opts.train_size))
+		else:
+			batch_sampler = None
+		self.trainloader = data.FauxDataLoader(self.trainset, sampler=batch_sampler, batch_size=self.opts.batch_size)
 		# Initialize testset
-		self.testset = data.Dataset(split='test', pairs='annotated', transform=transform)
-		self.testloaders = [data.FauxDataLoader(self.testset, self.opts.test_batch_size)]
+		if self.opts.do_validation:
+			self.testset = data.Dataset(split='test', pairs='annotated', transform=transform)
+			self.testloaders = [data.FauxDataLoader(self.testset, batch_size=self.opts.test_batch_size)]
+		else:
+			print('No testset')
+			self.testloaders = []
 
 if __name__ == '__main__':
 	r = Runner()

visualize/__main__.py
@@ -10,6 +10,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import os
 import sys
 import glob
 from optparse import OptionParser
@@ -18,18 +19,29 @@ import pdb
 
 
 
-pattern = re.compile(r'^\s*(.*?)\s*\(ep\s+(\d+):\s+(\d+)/\d+\)\s+loss (\S+)\s+acc (\S+)', re.MULTILINE)
-groups = [('name', np.str_, 16),
+
+regex = (
+	re.compile(r'^\s*(.*?)\s*\(ep\s+(\d+):\s+(\d+)/\d+\)\s+loss (\S+)\s+acc (\S+)', re.MULTILINE),
+	 [('name', np.str_, 16),
 	('epoch', np.int32),
 	('batch', np.int32),
 	('loss', np.float64),
 	('acc', np.float32)]
+)
 
+rec_regex = (
+	re.compile(r'^\s*(.*?)\s*\(ep\s+(\d+):\s+(\d+)/\d+\)\s+loss (\S+)\s+acc (\S+)\s+rec (\S+)', re.MULTILINE),
+	regex[1] + [('rec', np.float32)]
+)
 
 class Plotter(object):
 	def __init__(self, key, *fpaths):
 		self.key = key
 		self.labels = []
+		if key == 'rec':
+			self.regex = rec_regex
+		else:
+			self.regex = regex
 		for fpath in fpaths:
 			plt.figure(fpath)
 			self.ax = plt.subplot()
@@ -37,7 +49,7 @@ class Plotter(object):
 			self._line(fpath if type(fpath) is str else fpath[0])
 			self.ax.legend(self.labels)
 	def _line(self, fpath):
-		dat = np.fromregex(fpath, pattern, groups)
+		dat = np.fromregex(fpath, *self.regex)
 		srcs = set(dat['name'])
 		subs = {src: dat[dat['name'] == src] for src in srcs}
 		srcs = sorted(subs, key=lambda x: len(subs[x]), reverse=True)
@@ -46,12 +58,19 @@ class Plotter(object):
 			self.ax.plot(sub['batch'], sub[self.key])
 			self.labels.append(src)
 
+
+
 if __name__ == '__main__':
 	# Parse command line args
 	parser = OptionParser()
 	parser.add_option('-f', '--field', dest='field', default='acc')
+	parser.add_option('-l', '--last', dest='latest_only', action='store_true', default=False)
 	opts, args = parser.parse_args()
+	# Get logfiles
+	fpaths = glob.glob('log/*.log') if len(args) == 0 else args
+	if opts.latest_only:
+		latest_file = max(fpaths, key=os.path.getctime)
+		fpaths = [latest_file]
 	# Run
-	fpaths = glob.glob('log/*.log') if len(sys.argv) < 2 else sys.argv[1:]
-	Plotter(opts.field, *args)
+	Plotter(opts.field, *fpaths)
 	plt.show()